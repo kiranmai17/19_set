42c9ea2ed2a9ef438454318cfef595ab1433065f
Kothapelli Kiranmai <116304133+kiranmai17@users.noreply.github.com>
2022-11-09 18:57:18 +0530

    Add files via upload

---
+++ hello_world.py
@@ -0,0 +1,31 @@
+"""
+Day: Wednesday
+Date: 09th n ov2022
+Topic: type method , comments
+"""
+
+''' to print hello world'''
+print('hello world')
+print('hello world')
+print('hello world')
+print('hello world')
+print('hello world')
+
+print(type(-4))
+print(type(-8))
+print(type(0))
+print(type(32425346))
+
+print(3)
+print(7)
+print(9)
+print(79)
+print(399)
+print(239)
+print(2529)
+print(2329)
+
+print(type(-3.4))
+print(type(.6))
+print(type(0.9))
+print(type(3232.55))
